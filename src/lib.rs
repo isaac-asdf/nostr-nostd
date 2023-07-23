f2f0fd25c6963a78636c056e4b2e523ba1871a55
isaac-asdf <97187398+isaac-asdf@users.noreply.github.com>
2023-07-23 09:55:27 -0500

    Builder rework (#2)
    * set content and tags as optional
    
    * added setters, need to add struct
    
    * new type for builder added
    
    * remove ref for builder
    
    * added generic for time set
    
    * individual tag types
    
    * generic tag count impl!
    
    * better name for traint
    
    * use heapless vec for tags
    
    * Update lib.rs
    
    * rename to fn add_tag
    
    * use heapless for content and tags
    
    * docs wip
    
    * use heapless vec for return
    
    * docs
    
    * docs update
    
    * handle push errors
    
    * docs and vec push handle
    
    * docs
    
    * version to 0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,29 @@
 #![no_std]
-//! Implementation of [Nostr](https://nostr.com/) note creation for a no_std environment.
-//! Example project on an esp32 can be seen [here](https://github.com/isaac-asdf/esp32-nostr-client).
+//! Implementation of [Nostr](https://nostr.com/) note creation for a #![no_std] environment.
+//! An example project using an esp32 can be seen [here](https://github.com/isaac-asdf/esp32-nostr-client).
 //!
-//! # Examples
+//! # Example
 //! ```
-//! use nostr_nostd::Note;
+//! use nostr_nostd::{Note, String};
 //! const PRIVKEY: &str = "a5084b35a58e3e1a26f5efb46cb9dbada73191526aa6d11bccb590cbeb2d8fa3";
-//! let note = Note::new(PRIVKEY, "test note", 1686880020, [0; 32]);
-//! let (msg, len) = note.serialize_to_relay();
-//! let msg = &msg[0..len];
+//! let content: String<100> = String::from("Hello, World!");
+//! let tag: String<64> = String::from(r#"["relay", "wss://relay.example.com/"]"#);
+//! // aux_rand should be generated from a random number generator
+//! // required to keep PRIVKEY secure with Schnorr signatures
+//! let aux_rand = [0; 32];
+//! let note = Note::new()
+//!     .content(content)
+//!     .add_tag(tag)
+//!     .created_at(1686880020)
+//!     .build(PRIVKEY, aux_rand);
+//! let msg = note.serialize_to_relay();
 //! ```
 //!
-use heapless::String;
+pub use heapless::{String, Vec};
 use secp256k1::{self, ffi::types::AlignedType, KeyPair, Message};
 use sha2::{Digest, Sha256};
 
-/// Defined in [nost-protocol](https://github.com/nostr-protocol/nips/tree/master#event-kinds)
+/// Defined by the [nostr protocol](https://github.com/nostr-protocol/nips/tree/master#event-kinds)
 #[derive(Copy, Clone)]
 pub enum NoteKinds {
     /// For most short text based notes
@@ -49,31 +57,168 @@ pub struct Note {
     pubkey: [u8; 64],
     /// Unix timestamp
     created_at: u32,
-    /// Hardcoded to kind 1
+    /// Default to kind 1
     kind: NoteKinds,
-    tags: [[u8; 100]; 5],
-    content: String<64>,
+    tags: Vec<String<64>, 5>,
+    content: Option<String<100>>,
     sig: [u8; 128],
 }
 
-impl Note {
-    /// Returns a note used for authenticating to a relay
-    pub fn auth_note(privkey: &str, created_at: u32, aux_rnd: [u8; 32]) -> Self {
-        let mut note = Note {
-            id: [0; 64],
-            pubkey: [0; 64],
-            created_at,
-            kind: NoteKinds::Auth,
-            tags: [[255; 100]; 5],
-            content: "".into(),
-            sig: [0; 128],
-        };
-        note.set_pubkey(privkey);
-        note.set_id();
-        note.set_sig(privkey, &aux_rnd);
-        note
+/// NoteBuilder has had `.created_at()` called and is ready for `.build()`
+pub struct TimeSet;
+/// NoteBuilder is waiting for `.created_at()` prior to `.build()`
+pub struct TimeNotSet;
+
+/// Impl for tags which can had an additional tag added.
+/// ie, not implemented for FiveTags but implemented for all others
+pub trait AddTag {
+    type Next: TagCount;
+    // Add a generic parameter for the return type, bounded by TagCount
+    fn next(self) -> Self::Next;
+}
+/// Number of tags added
+pub trait TagCount {}
+/// No tags have been added
+pub struct ZeroTags;
+/// One tag has been added
+pub struct OneTag;
+/// Two tags have been added
+pub struct TwoTags;
+/// Three tags have been added
+pub struct ThreeTags;
+/// Four tags have been added
+pub struct FourTags;
+/// Five tags have been added
+pub struct FiveTags;
+
+impl TagCount for ZeroTags {}
+impl TagCount for OneTag {}
+impl TagCount for TwoTags {}
+impl TagCount for ThreeTags {}
+impl TagCount for FourTags {}
+impl TagCount for FiveTags {}
+
+impl AddTag for ZeroTags {
+    type Next = OneTag;
+    // Implement the next method to return a new MyType instance
+    fn next(self) -> OneTag {
+        OneTag
+    }
+}
+impl AddTag for OneTag {
+    type Next = TwoTags;
+    fn next(self) -> TwoTags {
+        TwoTags
+    }
+}
+impl AddTag for TwoTags {
+    type Next = ThreeTags;
+    fn next(self) -> ThreeTags {
+        ThreeTags
+    }
+}
+impl AddTag for ThreeTags {
+    type Next = FourTags;
+    fn next(self) -> FourTags {
+        FourTags
+    }
+}
+impl AddTag for FourTags {
+    type Next = FiveTags;
+    fn next(self) -> FiveTags {
+        FiveTags
+    }
+}
+
+/// Used to track the addition of the time created and the number of tags added
+pub struct BuildStatus<A, B> {
+    time: A,
+    tags: B,
+}
+
+/// Used to fill in the fields of a Note.
+pub struct NoteBuilder<A, B> {
+    build_status: BuildStatus<A, B>,
+    note: Note,
+}
+
+impl<A, T, NextAddTag> NoteBuilder<A, T>
+where
+    T: AddTag<Next = NextAddTag>,
+    NextAddTag: TagCount,
+{
+    /// Adds a new tag to the note.
+    /// The maximum number of tags currently allowed is 5.
+    /// Attempts to add too many tags will be a compilation error.
+    ///
+    /// # Example
+    /// ```
+    /// use nostr_nostd::{Note, String};
+    /// const PRIVKEY: &str = "a5084b35a58e3e1a26f5efb46cb9dbada73191526aa6d11bccb590cbeb2d8fa3";
+    /// let content: String<100> = String::from("i have tags");
+    /// let tag: String<64> = String::from(r#"["relay", "wss://relay.example.com/"]"#);
+    /// let note = Note::new()
+    ///     .content(content)
+    ///     .add_tag(tag)
+    ///     .created_at(1690076405)
+    ///     .build(PRIVKEY, [0; 32]);
+    /// ```
+
+    pub fn add_tag(mut self, tag: String<64>) -> NoteBuilder<A, NextAddTag> {
+        let next_tags = self.build_status.tags.next();
+        self.note
+            .tags
+            .push(tag)
+            .expect("AddTag impl error, should be impossible to err here");
+
+        NoteBuilder {
+            build_status: BuildStatus {
+                time: self.build_status.time,
+                tags: next_tags,
+            },
+            note: self.note,
+        }
+    }
+}
+
+impl<A, B> NoteBuilder<A, B> {
+    /// Sets the "kind" field of the note
+    pub fn set_kind(mut self, kind: NoteKinds) -> Self {
+        self.note.kind = kind;
+        self
     }
 
+    /// Sets the "content" field of Note
+    pub fn content(mut self, content: String<100>) -> Self {
+        self.note.content = Some(content);
+        self
+    }
+}
+
+impl<A> NoteBuilder<TimeNotSet, A> {
+    /// Must be called prior to build() to set the time of the note
+    pub fn created_at(mut self, created_at: u32) -> NoteBuilder<TimeSet, A> {
+        self.note.created_at = created_at;
+        NoteBuilder {
+            build_status: BuildStatus {
+                time: TimeSet,
+                tags: self.build_status.tags,
+            },
+            note: self.note,
+        }
+    }
+}
+
+impl<A> NoteBuilder<TimeSet, A> {
+    pub fn build(mut self, privkey: &str, aux_rnd: [u8; 32]) -> Note {
+        self.note.set_pubkey(privkey);
+        self.note.set_id();
+        self.note.set_sig(privkey, &aux_rnd);
+        self.note
+    }
+}
+
+impl Note {
     /// Returns a new Note
     /// # Arguments
     ///
@@ -81,25 +226,27 @@ impl Note {
     /// * `aux_rnd` - MUST be unique for each note created to avoid leaking private key
     /// * `created_at` - Unix timestamp for note creation time
     ///
-    pub fn new(privkey: &str, content: &str, created_at: u32, aux_rnd: [u8; 32]) -> Self {
-        let mut note = Note {
-            id: [0; 64],
-            pubkey: [0; 64],
-            created_at,
-            kind: NoteKinds::ShortNote,
-            tags: [[255; 100]; 5],
-            content: content.into(),
-            sig: [0; 128],
-        };
-        note.set_pubkey(privkey);
-        note.set_id();
-        note.set_sig(privkey, &aux_rnd);
-        note
+    pub fn new() -> NoteBuilder<TimeNotSet, ZeroTags> {
+        NoteBuilder {
+            build_status: BuildStatus {
+                time: TimeNotSet,
+                tags: ZeroTags,
+            },
+            note: Note {
+                id: [0; 64],
+                pubkey: [0; 64],
+                created_at: 0,
+                kind: NoteKinds::ShortNote,
+                tags: Vec::new(),
+                content: None,
+                sig: [0; 128],
+            },
+        }
     }
 
     fn timestamp_bytes(&self) -> [u8; 10] {
         // thanks to ChatGPT for the below code :)
-        let mut buffer = [0_u8; 10];
+        let mut buffer = [255_u8; 10];
         let mut idx = buffer.len();
         let mut n = self.created_at;
 
@@ -128,8 +275,10 @@ impl Note {
             count += 1;
         });
         self.timestamp_bytes().iter().for_each(|bs| {
-            hash_str[count] = *bs;
-            count += 1;
+            if *bs != 255 {
+                hash_str[count] = *bs;
+                count += 1;
+            }
         });
         hash_str[count] = 44; // 44 = ,
         count += 1;
@@ -147,21 +296,21 @@ impl Note {
             count += 1;
         });
         self.tags.iter().for_each(|tag| {
-            tag.iter().for_each(|bs| {
-                if *bs != 255 {
-                    hash_str[count] = *bs;
-                    count += 1;
-                }
+            tag.as_bytes().iter().for_each(|bs| {
+                hash_str[count] = *bs;
+                count += 1;
             })
         });
         br#"],""#.iter().for_each(|bs| {
             hash_str[count] = *bs;
             count += 1;
         });
-        self.content.as_bytes().iter().for_each(|bs| {
-            hash_str[count] = *bs;
-            count += 1;
-        });
+        if let Some(content) = &self.content {
+            content.as_bytes().iter().for_each(|bs| {
+                hash_str[count] = *bs;
+                count += 1;
+            });
+        }
         br#""]"#.iter().for_each(|bs| {
             hash_str[count] = *bs;
             count += 1;
@@ -185,7 +334,6 @@ impl Note {
         base16ct::lower::encode(&results, &mut self.id).expect("encode error");
     }
 
-    // todo: return signing error
     fn set_sig(&mut self, privkey: &str, aux_rnd: &[u8; 32]) {
         // figure out what size we need and why
         let mut buf = [AlignedType::zeroed(); 64];
@@ -200,101 +348,119 @@ impl Note {
         base16ct::lower::encode(sig.as_ref(), &mut self.sig).expect("encode error");
     }
 
-    fn to_json(&self) -> ([u8; 1200], usize) {
-        let mut output = [0; 1200];
-        let mut count = 0;
+    fn to_json(&self) -> Vec<u8, 1000> {
+        let mut output: Vec<u8, 1000> = Vec::new();
         br#"{"content":""#.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            // handle result?
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
-        self.content.as_bytes().iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
-        });
+        if let Some(content) = &self.content {
+            content.as_bytes().iter().for_each(|bs| {
+                output
+                    .push(*bs)
+                    .expect("Impossible due to size constraints of content, tags");
+            });
+        }
         br#"","created_at":"#.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
         self.timestamp_bytes().iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            if *bs != 255 {
+                output
+                    .push(*bs)
+                    .expect("Impossible due to size constraints of content, tags");
+            }
         });
         br#","id":""#.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
         self.id.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
         br#"","kind":"#.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
         self.kind.serialize().iter().for_each(|bs| {
             if *bs != 255 {
-                output[count] = *bs;
-                count += 1;
+                output
+                    .push(*bs)
+                    .expect("Impossible due to size constraints of content, tags");
             }
         });
         br#","pubkey":""#.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
         self.pubkey.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
         br#"","sig":""#.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
         self.sig.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
         br#"","tags":["#.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
         self.tags.iter().for_each(|tag| {
-            tag.iter().for_each(|bs| {
-                if *bs != 255 {
-                    output[count] = *bs;
-                    count += 1;
-                }
+            tag.as_bytes().iter().for_each(|bs| {
+                output
+                    .push(*bs)
+                    .expect("Impossible due to size constraints of content, tags");
             })
         });
         br#"]}"#.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
 
-        (output, count)
+        output
     }
 
-    /// Serializes the note so it can sent to a relay
+    /// Serializes the note so it can be sent to a relay
     /// # Returns
     ///
     /// * `[u8; 1000]` - lower case hex encoded byte array of note, to be sent to relay
     /// * `usize` - length of the buffer used
-    pub fn serialize_to_relay(&self) -> ([u8; 1000], usize) {
-        let mut output = [0; 1000];
-        let mut count = 0;
+    pub fn serialize_to_relay(self) -> Vec<u8, 1000> {
+        let mut output: Vec<u8, 1000> = Vec::new();
         // fill in output
         br#"["EVENT","#.iter().for_each(|bs| {
-            output[count] = *bs;
-            count += 1;
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
         });
         let json = self.to_json();
-        for i in 0..json.1 {
-            output[count] = json.0[i];
-            count += 1;
-        }
-        output[count] = 93; // 93 == ] character
-        count += 1;
-
-        (output, count)
+        json.iter().for_each(|bs| {
+            output
+                .push(*bs)
+                .expect("Impossible due to size constraints of content, tags");
+        });
+        output
+            .push(93)
+            .expect("Impossible due to size constraints of content, tags");
+        output
     }
 }
 
@@ -303,9 +469,16 @@ mod tests {
     use super::*;
     const PRIVKEY: &str = "a5084b35a58e3e1a26f5efb46cb9dbada73191526aa6d11bccb590cbeb2d8fa3";
 
+    fn get_note() -> Note {
+        Note::new()
+            .content("esptest".into())
+            .created_at(1686880020)
+            .build(PRIVKEY, [0; 32])
+    }
+
     #[test]
     fn pubkey_test() {
-        let note = Note::new(PRIVKEY, "esptest", 1686880020, [0; 32]);
+        let note = get_note();
         let pubkey = note.pubkey;
         assert_eq!(
             pubkey,
@@ -315,7 +488,7 @@ mod tests {
 
     #[test]
     fn id_test() {
-        let note = Note::new(PRIVKEY, "esptest", 1686880020, [0; 32]);
+        let note = get_note();
         let id = note.id;
         assert_eq!(
             id,
@@ -325,7 +498,7 @@ mod tests {
 
     #[test]
     fn timestamp_test() {
-        let note = Note::new(PRIVKEY, "esptest", 1686880020, [0; 32]);
+        let note = get_note();
         let hash_correct = *b"1686880020";
         let ts = note.timestamp_bytes();
         assert_eq!(ts, hash_correct);
@@ -333,7 +506,7 @@ mod tests {
 
     #[test]
     fn hashstr_test() {
-        let note = Note::new(PRIVKEY, "esptest", 1686880020, [0; 32]);
+        let note = get_note();
         let hash_correct = br#"[0,"098ef66bce60dd4cf10b4ae5949d1ec6dd777ddeb4bc49b47f97275a127a63cf",1686880020,1,[],"esptest"]"#;
         let (hashed, len) = note.to_hash_str();
         let hashed = &hashed[..len];
@@ -343,16 +516,16 @@ mod tests {
     #[test]
     fn json_test() {
         let output =  br#"{"content":"esptest","created_at":1686880020,"id":"b515da91ac5df638fae0a6e658e03acc1dda6152dd2107d02d5702ccfcf927e8","kind":1,"pubkey":"098ef66bce60dd4cf10b4ae5949d1ec6dd777ddeb4bc49b47f97275a127a63cf","sig":"89a4f1ad4b65371e6c3167ea8cb13e73cf64dd5ee71224b1edd8c32ad817af2312202cadb2f22f35d599793e8b1c66b3979d4030f1e7a252098da4a4e0c48fab","tags":[]}"#;
-        let note = Note::new(PRIVKEY, "esptest", 1686880020, [0; 32]);
-        let (msg, len) = note.to_json();
-        assert_eq!(&msg[0..len], output);
+        let note = get_note();
+        let msg = note.to_json();
+        assert_eq!(&msg, output);
     }
 
     #[test]
     fn serialize_to_relay_test() {
         let output =  br#"["EVENT",{"content":"esptest","created_at":1686880020,"id":"b515da91ac5df638fae0a6e658e03acc1dda6152dd2107d02d5702ccfcf927e8","kind":1,"pubkey":"098ef66bce60dd4cf10b4ae5949d1ec6dd777ddeb4bc49b47f97275a127a63cf","sig":"89a4f1ad4b65371e6c3167ea8cb13e73cf64dd5ee71224b1edd8c32ad817af2312202cadb2f22f35d599793e8b1c66b3979d4030f1e7a252098da4a4e0c48fab","tags":[]}]"#;
-        let note = Note::new(PRIVKEY, "esptest", 1686880020, [0; 32]);
-        let (msg, len) = note.serialize_to_relay();
-        assert_eq!(&msg[0..len], output);
+        let note = get_note();
+        let msg = note.serialize_to_relay();
+        assert_eq!(&msg, output);
     }
 }
